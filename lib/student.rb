5a7c752b78948ecd55a05bf4a1b11078681d42bc
Karl Amoguis <ktamoguis@gmail.com>
2018-06-03 07:03:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
  def initialize(name, grade, id=nil)
-   @name = name:
-   @grade = grade:
+   @name = name
+   @grade = grade
  end
 
 end
